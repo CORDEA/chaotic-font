4cda83829baff59b99180f457aba3578e1e231d6
Yoshihiro Tanaka <contact@cordea.jp>
2022-10-29 13:53:40 +0900

    generate randomized indices

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const {load} = require('opentype.js');
 
+function randomizedIndices(length) {
+    const result = [];
+    while (result.length < length) {
+        const r = Math.floor(Math.random() * length);
+        if (result.includes(r)) {
+            continue;
+        }
+        result.push(r);
+    }
+    return result;
+}
+
 function randomize(glyphs) {
+    const indices = randomizedIndices(glyphs.length);
     const result = [];
     for (let i = 0; i < glyphs.length; i++) {
         const source = glyphs.get(i);
+        const r = glyphs.get(indices[i]);
         result.push(source);
     }
     return result;
