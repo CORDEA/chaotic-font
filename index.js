c0f32c5f12c7641bc45184cebb3ad96f81e792f2
Yoshihiro Tanaka <contact@cordea.jp>
2022-10-30 14:47:22 +0900

    randomize only alphabets

--- index.js
+++ index.js
@@ -32,23 +32,39 @@ function randomizedIndices(length) {
     return result;
 }
 
-function randomize(glyphs) {
-    const indices = randomizedIndices(glyphs.length);
+function randomize(glyphs, onlyAlphabet) {
+    const source = [];
+    const remaining = [];
+    if (onlyAlphabet) {
+        for (let i = 0; i < glyphs.length; i++) {
+            const c = glyphs.get(i);
+            if (0x41 <= c.unicode && c.unicode <= 0x5a || 0x61 <= c.unicode && c.unicode <= 0x7a) {
+                source.push(c);
+                continue;
+            }
+            remaining.push(c);
+        }
+    } else {
+        source.push(...glyphs);
+    }
+
+    const indices = randomizedIndices(source.length);
     const result = [];
     const map = new GlyphMap();
-    for (let i = 0; i < glyphs.length; i++) {
-        const source = glyphs.get(i);
-        const r = glyphs.get(indices[i]);
+    for (let i = 0; i < source.length; i++) {
+        const c = source[i];
+        const r = source[indices[i]];
         result.push(
             new Glyph({
-                name: source.name,
-                unicode: source.unicode,
+                name: c.name,
+                unicode: c.unicode,
                 path: r.path,
                 advanceWidth: r.advanceWidth
             })
         );
-        map.set(source.name, r.name);
+        map.set(c.name, r.name);
     }
+    result.push(...remaining);
     return new RandomizedGlyphs(result, map);
 }
 
@@ -79,18 +95,16 @@ function randomize(glyphs) {
     }
 
     const source = await load(argv.font);
-    if (argv._.includes('random')) {
-        const glyphs = randomize(source.glyphs);
-        const font = new Font({
-            familyName: source.names.fontFamily.en,
-            styleName: source.names.fontSubfamily.en,
-            unitsPerEm: source.unitsPerEm,
-            ascender: source.ascender,
-            descender: source.descender,
-            glyphs: glyphs.glyphs
-        });
-        font.download();
-        const json = JSON.stringify(glyphs.map.forJson());
-        await writeFile('glyphs.json', json);
-    }
+    const glyphs = randomize(source.glyphs, argv._.includes('ascii'));
+    const font = new Font({
+        familyName: source.names.fontFamily.en,
+        styleName: source.names.fontSubfamily.en,
+        unitsPerEm: source.unitsPerEm,
+        ascender: source.ascender,
+        descender: source.descender,
+        glyphs: glyphs.glyphs
+    });
+    font.download();
+    const json = JSON.stringify(glyphs.map.forJson());
+    await writeFile('glyphs.json', json);
 })();
